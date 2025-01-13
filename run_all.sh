e32a65662e3a36f75979d0b1a323c3ccb7763bf0
Laura Liparulo <lauratailz@yahoo.it>
2025-01-13 23:48:31 +0100

    sleep command added to give time to load the app in the browser

--- run_all.sh
+++ run_all.sh
@@ -6,11 +6,12 @@ CLUSTER_ID=$(echo "${CLUSTER_NAME}" | tr --delete \")
 REGION_OUT=$(terraform output region)
 REGION=$(echo "${REGION_OUT}" | tr --delete \")
 
-#install eksctl
+# install eksctl
 # curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 # sudo mv /tmp/eksctl /usr/local/bin
-# eksctl version
+#eksctl version
 
 aws eks --region $REGION update-kubeconfig --name $CLUSTER_ID
-kubectl apply -f k8s/nginx_namespace.yaml -f k8s/nginx_deployment.yaml -f k8s/nginx_service.yaml
+skubectl apply -f k8s/nginx_namespace.yaml -f k8s/nginx_deployment.yaml -f k8s/nginx_service.yaml
+sleep 1
 kubectl get service nginx-service --namespace nginx-ns --output='jsonpath={.status.loadBalancer.ingress[0].hostname}' && open http://$(kubectl get service nginx-service --namespace nginx-ns --output='jsonpath={.status.loadBalancer.ingress[0].hostname}')
